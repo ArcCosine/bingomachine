89ef76e603ea1c3b169a51ef9164becc8a98baf2
ArcCosine <arc@tunsns.net>
2017-05-16 02:55:58 +0900

    Update codes. remove jquery:)

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-#bingomachine
+# bingomachine
 
-# base
+It's simple bingo code.
+
+# base code
 
 [https://qiita.com/Yuta_Fujiwara/items/076b6ea61f456d9649e9](JavaScriptでビンゴマシンを作ってみた。)
 
 
+# start local server
 
+    node server.js
+
+Access to http://localhost:8888
--- js/Bingo.js
+++ js/Bingo.js
@@ -1,136 +1,141 @@
 (function(){
   'use strict';
+    function Bingo(num){
+        this.init(num);
+    }
 
-    //ビンゴの数字の数 5×5 = 75
-    var Bingo = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
-                   '11','12', '13', '14', '15', '16', '17', '18', '19', '20',
-                   '21','22', '23', '24', '25', '26', '27', '28', '29', '30',
-                   '31','32', '33', '34', '35', '36', '37', '38', '39', '40',
-                   '41','42', '43', '44', '45', '46', '47', '48', '49', '50',
-                   '51','52', '53', '54', '55', '56', '57', '58', '59', '60',
-                   '61','62', '63', '64', '65', '66', '67', '68', '69', '70',
-                   '71','72', '73', '74', '75'];
+    Bingo.prototype = {
+        init: function(num){
+            // create bingo array
+            this.bingoArray = (function(from, to) {
+                for(var tmp = []; from <= to; from++) {
+                    tmp.push(from);
+                }
+                return tmp;
+            })(1, num);
 
-    var timer = null;
-    var isPlaying = false;
+            // initialize value
+            this.timer = null;
+            this.isPlaying = false;
+            // selected bingonumber
+            this.bingoNum  = 0;
 
-    //乱数によってセレクトされた数字
-    var bingoNum = 0;
+            // get elements
+            this.leftPanel = document.getElementById("left-panel");
+            this.rightPanel = document.getElementById("right-panel");
+            this.stopButton = document.getElementById("stop-btn");
+            this.spinButton = document.getElementById("spinButton");
 
-    //bingoNumでセレクトされた数字をBingo配列から削除する変数
-    var delBingoNum = 0;
+            // sound manager
+            this.soundManager = this.getSoundObject();
 
-    var panel1 = document.getElementById('panel1');
-    var panel10 = document.getElementById('panel10');
-
-    var btn0 = document.getElementById('btn0');
-
-    var spinButton = document.getElementById('spinButton');
-
-    var soundManager = SoundManager();
-
-
-    // ビンゴの結果エリアのレンダリング
-    var renderBingo = function(){
-        var fragment= document.createDocumentFragment();
-        var divWrapper;
-        Bingo.forEach(function(elem, index){
-            if( index % 15 === 0 ){
-                divWrapper = fragment.appendChild(document.createElement("div"));
+            // do event
+            this.renderBingo();
+            this.bindEvent();
+        },
+        renderBingo: function(){
+            var fragment= document.createDocumentFragment();
+            var _self = this;
+            var divWrapper;
+            this.bingoArray.forEach(function(elem, index){
+                if( index % 15 === 0 ){
+                    divWrapper = fragment.appendChild(document.createElement("div"));
+                }
+                var numDiv = divWrapper.appendChild(document.createElement("div"));
+                numDiv.className = "bingo";
+                numDiv.textContent = _self.showNumber(elem);
+            });
+            document.getElementById("result").appendChild(fragment);
+        },
+        bindEvent: function(){
+            var _self = this;
+            this.spinButton.addEventListener("click", function(){ _self.clickSpin(); }, false );
+            this.stopButton.addEventListener("click", function(){ _self.stopSlot(); }, false );
+        },
+        clickSpin: function(){
+            if (this.isPlaying){
+                return;
             }
-            var numDiv = divWrapper.appendChild(document.createElement("div"));
-            numDiv.className = "bingo";
-            numDiv.innerHTML = elem;
-        });
-        var result = document.getElementById("result");
-        result.appendChild(fragment);
-    };
-    renderBingo();
+            this.soundManager.playDrum();
+
+            this.isPlaying = true;
+            this.spinButton.className = "inactive";
+            this.stopButton.className = "btn";
+
+            var _self = this;
+            this.timer = setInterval( function(){ _self.showRandom(); } , 25 );
+        },
+        showRandom: function(){
+            this.bingoNum = Math.floor(Math.random() * this.bingoArray.length);
+            var showText = this.showNumber(this.bingoArray[this.bingoNum]);
+            this.leftPanel.textContent = showText.substr(0,1);
+            this.rightPanel.textContent = showText.substr(1,2);
+        },
+        showNumber: function(number){
+            return (number < 10 ? "0" : "") + number;
+        },
+        stopSlot: function(){
+            if (!this.isPlaying){
+                return;
+            }
+            this.stopButton.className = "btn inactive";
+
+            this.soundManager.stopDrum();
+            this.soundManager.playCymbal();
+
+            clearInterval(this.timer);
+
+            // change color
+            var bingoDiv = document.querySelectorAll(".bingo");
+            var tBingo = this.bingoArray[this.bingoNum]-1;
+            bingoDiv[tBingo].textContent = this.showNumber(this.bingoArray[this.bingoNum]);
+            bingoDiv[tBingo].className = "bingo unmatched";
+
+            // delete selected number
+            this.bingoArray.splice(this.bingoNum,1);
+
+            // init
+            this.isPlaying = false;
+            this.timer = null;
+            this.spinButton.className = "";
+
+        },
+        getSoundObject: function(){
+            var drum = document.getElementById("audio_drum");
+            var cymbal = document.getElementById("audio_cymbal");
+            return {
+                playDrum    : playDrum,
+                stopDrum    : stopDrum,
+                playCymbal  : playCymbal
+            };
 
 
+            function playDrum(){
+                if(drum === null){
+                    return;
+                }
+                drum.currentTime = 0;
+                drum.play();
+            }
 
-    //『SPIN』ボタン押下
-    spinButton.addEventListener('click', function() {
-        if (isPlaying) return;
+            function stopDrum(){
+                if(drum === null){
+                    return;
+                }
+                drum.pause();
+            }
 
-        soundManager.playDrum();
-
-        isPlaying = true;
-        spinButton.className = 'inactive';
-        btn0.className = 'btn';
-
-        timer = setInterval(function(){
-            //『SPIN』ボタン押下後の処理
-            // ランダム表示
-            var num = Math.floor(Math.random() * Bingo.length);
-            bingoNum = num;
-            panel1.innerHTML = Bingo[num].substr(0,1);
-            panel10.innerHTML = Bingo[num].substr(1,2);
-        } , 25 );
-
-    });
-
-    btn0.addEventListener('click', function() {
-        stopSlot(0, this);
-    });
-
-    //『STOP』ボタン押下後の処理
-    function stopSlot(n, btn) {
-        if (!isPlaying) return;
-        btn.className = 'btn inactive';
-
-        soundManager.stopDrum();
-        soundManager.playCymbal();
-
-        clearInterval(timer);
-
-        delBingoNum = Bingo.indexOf(bingoNum);
-        Bingo.splice(delBingoNum,1);
-
-        var bingoDiv = document.querySelectorAll(".bingo");
-
-        // change color
-        var tBingo = Number(Bingo[bingoNum]);
-        bingoDiv[tBingo-1].innerHTML = Bingo[bingoNum];
-        bingoDiv[tBingo-1].className = 'bingo unmatched';
-
-        // init
-        isPlaying = false;
-        timer = null;
-        spinButton.className = '';
-    }
-
-    function SoundManager(){
-        var drum = document.getElementById('audio_drum');
-        var cymbal = document.getElementById('audio_cymbal');
-        return {
-          playDrum    : playDrum,
-          stopDrum    : stopDrum,
-          playCymbal  : playCymbal
-        }
-
-        function playDrum(){
-          if(drum == null){
-            return;
-          }
-          drum.currentTime = 0;
-          drum.play();
-        }
-
-        function stopDrum(){
-          if(drum == null){
-            return;
-          }
-          drum.pause();
-        }
-
-        function playCymbal(){
-          if(cymbal == null){
-            return;
-          }
-          cymbal.currentTime = 0;
-          cymbal.play();
+            function playCymbal(){
+                if(cymbal === null){
+                    return;
+                }
+                cymbal.currentTime = 0;
+                cymbal.play();
+            }
         }
     }
+
+    // start 75
+    new Bingo(75);
 
 })();
